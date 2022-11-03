b330e7bebd0fbd62d5fc9f868dc1415eaf71d1fc
magdaswiecka <magstaron@gmail.com>
2022-11-03 17:47:43 +0100

    Zadanie 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
 
+let timerID;
+
+buttonStart.addEventListener('click', () => {
+  buttonStart.disabled = true;
+
+  timerID = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+buttonStop.addEventListener('click', () => {
+  buttonStart.disabled = false;
+  clearInterval(timerID);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
